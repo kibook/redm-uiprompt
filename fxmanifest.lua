15596b83df515a28067bf57770f23062e09efcaa
kibook <kibuzhj@gmail.com>
2021-08-28 15:51:31 -0400

    Update fx_version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version "adamant"
+fx_version "cerulean"
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
